63974eb0d1dcd3a3a3987e0013a8d38cbd04af3b
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-17 22:18:17 +0330

    make navber looks better

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,22 +5,22 @@ const Navbar: React.FC = () => {
   return (
     <NavbarFlowbite className="p-5">
       <NavbarFlowbite.Brand href="https://flowbite-react.com">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        <span className="self-center whitespace-nowrap text-3xl md:text-xl font-semibold dark:text-white">
           Posts
         </span>
       </NavbarFlowbite.Brand>
       <NavbarFlowbite.Toggle />
       <NavbarFlowbite.Collapse>
-        <a className="text-purple-500" href="#">
+        <a className="text-purple-500 text-xl md:text-sm" href="#">
           Posts
         </a>
-        <a className="hover:text-purple-400" href="#">
+        <a className="hover:text-purple-400 text-xl md:text-sm" href="#">
           Logout
         </a>
-        <a className="hover:text-purple-400" href="#">
+        <a className="hover:text-purple-400 text-xl md:text-sm" href="#">
           Login
         </a>
-        <a className="hover:text-purple-400" href="#">
+        <a className="hover:text-purple-400 text-xl md:text-sm" href="#">
           Signup
         </a>
       </NavbarFlowbite.Collapse>
